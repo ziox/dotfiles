1713099fd7bf2d006d256dd6f61c5fb457c0f574
Nicola Pasta <nicola.pasta@gmail.com>
2014-10-14 15:00:34 +0200

    Enable CUA mode

--- emacs.d/init-gui.el
+++ emacs.d/init-gui.el
@@ -3,3 +3,9 @@
 ;; MouseWheel!
 (setq mouse-wheel-scroll-amount '(2 ((shift) . 4) ((control) . nil)))
 (setq mouse-wheel-progressive-speed nil)
+
+;; CUA mode for fuck sake!
+(cua-mode t)
+(setq cua-auto-tabify-rectangles nil)
+(transient-mark-mode 1)
+(setq cua-keep-region-after-copy t)
