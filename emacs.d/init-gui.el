90d25678c86602836ad9c7226a633fe42b61f75a
Nicola Pasta <nicola.pasta@gmail.com>
2015-01-29 15:12:59 +0100

    Emacs: switch to another theme

--- emacs.d/init-gui.el
+++ emacs.d/init-gui.el
@@ -3,7 +3,8 @@
 ;; Theme & Fonts
 ; https://github.com/bbatsov/zenburn-emacs
 (use-package color-theme-sanityinc-solarized :ensure)
-(load-theme 'sanityinc-solarized-dark t)
+; (load-theme 'sanityinc-solarized-dark t)
+(load-theme 'misterioso)
 (set-cursor-color "lightgreen")
 
 ;; Linum Mode
