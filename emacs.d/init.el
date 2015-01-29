dfb2439ad0e0225cef99a04d429db525e2406648
Nicola Pasta <nicola.pasta@gmail.com>
2015-01-29 13:45:30 +0100

    Emacs: enable uniquify

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -52,6 +52,13 @@
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
+;; Uniquify buffer names
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'reverse)
+(setq uniquify-separator "/")
+(setq uniquify-after-kill-buffer-p t) ; rename after killing uniquified
+(setq uniquify-ignore-buffers-re "^\\*") ; don't muck with special buffers
+
 ;; Load packages settings
 (load "~/.emacs.d/init-packages.el")
 
